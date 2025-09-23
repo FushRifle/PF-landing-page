7cfccd50ab1443ca5926be2364f60407e95d2efc
Abdullahi Olawale Mukaila <ayomikail21@gmail.com>
2025-09-23 19:48:10 +0100

    icon and siteDetails changed

--- src/data/siteDetails.ts
+++ src/data/siteDetails.ts
@@ -3,10 +3,10 @@ export const siteDetails = {
     siteUrl: 'https://parentfully.app/',
     metadata: {
         title: 'ParentFully',
-        description: 'ParentFully empowers businesses with cutting-edge technology solutions to drive success and efficiency.',
+        description: 'ParentFully empowers parents with cutting-edge technology and solutions to drive success and efficiency.',
     },
     language: 'en-us',
     locale: 'en-US',
-    siteLogo: `${process.env.BASE_PATH || ''}/images/logo.png`, // or use a string for the logo e.g. "TechStartup"
-    googleAnalyticsId: '', // e.g. G-XXXXXXX,
+    siteLogo: `${process.env.BASE_PATH || ''}/images/icon.png`,
+    googleAnalyticsId: '',
 }
