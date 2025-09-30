a8836f2531349233def5df6df85f794b6cceaf73
FushRifle <ayomikail21@gmail.com>
2025-09-30 20:23:12 +0100

    icon and site Details

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -1,5 +1,5 @@
 export const heroDetails = {
     heading: 'Two Parents. One App. Zero Conflict.',
-    subheading: 'Parentfully helps parents and co-parents align, plan, and focus on what matters most—raising thriving kids—whether you live together or apart.',
+    subheading: 'Parentfully helps parents and co-parents align, plan, and focus on what matters most, raising thriving kids, whether you live together or apart.',
     centerImageSrc: '/images/Hero.png',
 }
