9ef9505b2d2fbf011e9f160d71ef33f173a67ca2
FushRifle <ayomikail21@gmail.com>
2026-03-28 02:21:07 +0100

    siteDetails

--- src/data/siteDetails.ts
+++ src/data/siteDetails.ts
@@ -1,12 +1,12 @@
 export const siteDetails = {
     siteName: 'ParentFully',
     siteUrl: 'https://parentfully.app/',
-    metadata: {
-        title: 'ParentFully',
-        description: 'ParentFully empowers parents with cutting-edge technology and solutions to drive success and efficiency.',
-    },
     language: 'en-us',
     locale: 'en-US',
     siteLogo: `${process.env.BASE_PATH || ''}/images/icon.png`,
     googleAnalyticsId: '',
-}
+    metadata: {
+        title: 'ParentFully',
+        description: 'Parentfully is a free parenting and child development app designed to help parents and co-parents raise confident, responsible, and thriving children with more structure and intention. Inside the app you can set goals for your child, track growth and progress, guide habits and character development, stay aligned and parent more intentionally instead of reacting every day.',
+    },
+};
